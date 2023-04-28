489bd27a946bb9510f9f66b1613b1e22ef19cbc8
Dusty-Bshipgit config --global user.name Dusty-Bshipgit config --global user.name Dusty-Bship <dustydjb10@gmail.com>
2023-04-28 14:34:11 -0400

    Completed phase04

--- phase04/private/db_credentials.php
+++ phase04/private/db_credentials.php
@@ -1,12 +1,12 @@
 <?php
 // local
-define("DB_SERVER", "localhost");
-define("DB_USER", "sally");
-define("DB_PASS", "somepa55word");
-define("DB_NAME", "salamanders");
+//define("DB_SERVER", "");
+//define("DB_USER", "");
+//define("DB_PASS", "");
+//define("DB_NAME", "");
 
-//a2
-// define("DB_SERVER", "localhost");
-// define("DB_USER", "charli12_doall");
-// define("DB_PASS", "ErYje7HV2o2Lqp7B9gx");
-// define("DB_NAME", "charli12_salamanders");
+//sitegrounds
+//define("DB_SERVER", "");
+//define("DB_USER", "");
+//define("DB_PASS", "");
+//define("DB_NAME", "");
--- phase04/public/salamanders/delete.php
+++ phase04/public/salamanders/delete.php
@@ -8,7 +8,10 @@ if(!isset($_GET['id'])) {
   $salamander = find_salamander_by_id($id);
   
   if(is_post_request()) {
-  
+    delete_salamander($id);
+    redirect_to(url_for('salamanders/index.php'));
+  } else {
+    $salamander = find_salamander_by_id($id);
   }
   
   $pageTitle = 'Delete Salamander'; ?>
